0cddb5ddf0693d62130ade31259dbb2b0be0edbe
Kevin Heaton <kevin.heaton21@yahoo.com>
2022-04-30 11:37:49 -0500

    tables seed correctly and started department routes

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
+  database: 'employees',
   port: 3306
 });
 
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const routes = require('./routes');
+// const db = require('./config/connection');
+const routes = require('./routes/api');
 const sequelize = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // turn on routes
-app.use(routes);
+app.use('/api', routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
